659e3324acb1c1bf5d30d610f3b46638fdc04b27
Günter Hipler <guenter.hipler@unibas.ch>
2020-11-18 21:26:26 +0100

    escape character2

--- src/main/g8/templateTypes/simpleLoader/Main.scala
+++ src/main/g8/templateTypes/simpleLoader/Main.scala
@@ -17,8 +17,8 @@ object Main extends Logging{
     false)
 
 
-  logger.info("starting service with the following config settings")
-  settings.getAppSettings.forEach((key,value) => logger.info(s"key: $key  value: $value"))
+  //logger.info("starting service with the following config settings")
+  //settings.getAppSettings.forEach((key,value) => logger.info(s"key: $key  value: $value"))
 
 
 
